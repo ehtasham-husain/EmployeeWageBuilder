3b18167edacc85146b3580a3beec06dd5d92aae0
Ehtasham <ehtashamhusain@gmail.com>
2021-08-12 23:36:47 +0530

    UC-3 Add Part time employee wage

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -1,17 +1,20 @@
 public class EmployeeWageBuilder {
     public static void main(String[] args) {
         //Constants
-        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
         //variables
         int empHrs = 0;
         int empWage = 0;
 
         //Computation
-        int EmpCheck = (int) (Math.floor(Math.random() * 10) % 2);
+        int EmpCheck = (int) (Math.floor(Math.random() * 10) % 3);
         System.out.println(EmpCheck);
         if (EmpCheck == IS_FULL_TIME)
             empHrs = 8;
+        else if (EmpCheck == IS_PART_TIME)
+            empHrs = 4;
 
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Total Employee Wage = "+empWage);
