6e441253d73321c9dcaf4e8f5cc69f21a9f6dd49
Ehtasham <ehtashamhusain@gmail.com>
2021-08-13 10:48:19 +0530

    UC-5 Calculate wage for a month assume 20 working days

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -3,27 +3,31 @@ public class EmployeeWageBuilder {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 10;
 
     public static void main(String[] args) {
 
         //variables
-        int empHrs = 0;
-        int empWage = 0;
+        int empHrs = 0, empWage = 0, totalEmpWage = 0;
 
         //Computation
-        int EmpCheck = (int) (Math.floor(Math.random() * 10) % 3);
+        for (int days = 1; days <= NUM_OF_WORKING_DAYS; days++ ) {
+            int EmpCheck = (int) (Math.floor(Math.random() * 10) % 3);
+            System.out.println(EmpCheck);
 
-        switch (EmpCheck) {
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            default:
-                empHrs = 0;
+            switch (EmpCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalEmpWage += empWage;
         }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                System.out.println("Total Employee Wage = " + empWage);
+        System.out.println("Total Employee Wage = " + totalEmpWage);
     }
 }
