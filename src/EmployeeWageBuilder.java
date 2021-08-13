858e3db416b23b40a06ad9eef5fb7777a4decb34
Ehtasham <ehtashamhusain@gmail.com>
2021-08-13 09:12:05 +0530

    UC-4 Calculating Employee Wage Using Switch Case

--- src/EmployeeWageBuilder.java
+++ src/EmployeeWageBuilder.java
@@ -1,22 +1,29 @@
 public class EmployeeWageBuilder {
+    //Constants
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+
     public static void main(String[] args) {
-        //Constants
-        int IS_PART_TIME = 1;
-        int IS_FULL_TIME = 2;
-        int EMP_RATE_PER_HOUR = 20;
+
         //variables
         int empHrs = 0;
         int empWage = 0;
 
         //Computation
         int EmpCheck = (int) (Math.floor(Math.random() * 10) % 3);
-        System.out.println(EmpCheck);
-        if (EmpCheck == IS_FULL_TIME)
-            empHrs = 8;
-        else if (EmpCheck == IS_PART_TIME)
-            empHrs = 4;
 
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Total Employee Wage = "+empWage);
+        switch (EmpCheck) {
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
+        }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                System.out.println("Total Employee Wage = " + empWage);
     }
 }
